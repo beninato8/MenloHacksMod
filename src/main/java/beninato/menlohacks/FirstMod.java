019be7e70d87063b10b3f59e5d56ccc35b0d524e
Nicholas Beninato <Nicholas@Nicholas-MacBook-Pro.local>
2017-03-18 03:40:01 -0700

    v3

--- src/main/java/beninato/menlohacks/FirstMod.java
+++ src/main/java/beninato/menlohacks/FirstMod.java
@@ -1,11 +1,42 @@
 package beninato.menlohacks;
 
+import beninato.menlohacks.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, 
 name = Reference.NAME, 
 version = Reference.VERSION, 
 acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
-public class FirstMod {
+public class FirstMod 
+{
+	@Instance
+	public static FirstMod instance;
 
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, 
+			serverSide = Reference.SERVER_PROXY_CLASS)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		System.out.println("PreInit");
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		System.out.println("Init");
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		System.out.println("PostInit");
+	}
 }
--- src/main/java/beninato/menlohacks/Reference.java
+++ src/main/java/beninato/menlohacks/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 	public static final String VERSION = "1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.9.4]";
 	
-	public static final String CLIENT_PROXY_CLASS = "";
-	public static final String SERVER_PROXY_CLASS = "";
+	public static final String CLIENT_PROXY_CLASS = "beninato.menlohacks.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "beninato.menlohacks.proxy.ServerProxy";
 
 }
